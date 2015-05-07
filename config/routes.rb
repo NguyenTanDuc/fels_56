5cd92537236e5600d3a48b287de985045c110492
NguyenTanDuc <nguyen.tan.duc@framgia.com>
2015-05-07 15:51:59 +0700

    UI word list, categories and controller skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  resources :users, except: [:index] 
+  root      'static_pages#home'
   get  'sign_up' => 'users#new'
+  resources :users, except: [:index]   
+  resources :words
+  resources :categories
+  resources :lessons, except: [:edit, :update]
 end

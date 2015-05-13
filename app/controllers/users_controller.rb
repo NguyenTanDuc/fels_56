131cbd4b1b986c166acf37e956d7b8f6890434d1
Dao Minh Duc <brokenheart1712@gmail.com>
2015-05-13 11:30:16 +0700

    Showing All Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def show
     @user = User.find params[:id]
   end
@@ -17,7 +21,7 @@ class UsersController < ApplicationController
       flash[:success] = t :welcome
       redirect_to @user
     else
-      render 'new'
+      render :new
     end
   end
 
@@ -31,7 +35,7 @@ class UsersController < ApplicationController
       flash[:success] = t :profile_updated
       redirect_to @user
     else
-      render 'edit'
+      render :edit
     end
   end
 

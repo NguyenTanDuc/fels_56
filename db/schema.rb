06a7e88e77f10ae6c92b8dbe6c761faa3482fe2b
Dao Minh Duc <brokenheart1712@gmail.com>
2015-05-14 17:35:57 +0700

    Pasword Reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513091457) do
+ActiveRecord::Schema.define(version: 20150514092657) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -87,6 +87,8 @@ ActiveRecord::Schema.define(version: 20150513091457) do
     t.string   "activation_digest"
     t.boolean  "activated",             default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   create_table "words", force: :cascade do |t|
